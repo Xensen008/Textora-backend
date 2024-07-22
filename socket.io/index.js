f66bf32e02b46200dead5ba04dac02600f574540
Xensen008 <arnabjyotikakati008@gmail.com>
2024-07-22 18:36:30 +0530

    Refactor socket.io integration and fix user disconnection bug and setup sidebar user showcase with unseen msg

--- socket.io/index.js
+++ socket.io/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { Server } = require('socket.io');
 const http = require('http');
 const app = express();
-const userDataToken = require('../utils/extractToken');  
+const userDataToken = require('../utils/extractToken');
 const UserModel = require('../models/user.model');
-require('dotenv').config(); 
+require('dotenv').config();
 const { ConvoModel, MessageModel } = require('../models/convo.model');
 
 const server = http.createServer(app);
@@ -17,37 +17,49 @@ const io = new Server(server, {
     }
 });
 // online status
-const onlineUser=  new Set()
+const onlineUser = new Set()
 
 // Handle socket connection
 io.on("connection", async (socket) => {
     console.log("User connected:", socket.id);
 
-    
+
     const token = socket?.handshake.auth.token;
     const user = await userDataToken(token);
-    
+
     //create a room
     socket.join(user?._id?.toString());
     onlineUser.add(user?._id?.toString())
 
     io.emit('onlineUser', Array.from(onlineUser));
-    
-    socket.on('message-page',async(userId)=>{
+
+    socket.on('message-page', async (userId) => {
         // console.log('userId',userId)
-        const userDetails= await UserModel.findById(userId).select("-password")
+        const userDetails = await UserModel.findById(userId).select("-password")
 
         const payload = {
-            _id :userDetails?._id,
-            name : userDetails?.name,
+            _id: userDetails?._id,
+            name: userDetails?.name,
             email: userDetails?.email,
             profile_pic: userDetails?.profile_pic,
             online: onlineUser.has(userId),
         }
-        socket.emit('message-user',payload)
-        
+        socket.emit('message-user', payload)
+
+        //get previous message load
+        const getConversation = await ConvoModel.findOne({
+            $or: [
+                { sender: user?._id, receiver: userId },
+                { sender: userId, receiver: user?._id },
+            ]
+        }).populate('messages').sort({ updatedAt: -1 });
+
+        socket.emit('message', getConversation.messages)
+
     })
 
+
+    //new message
     socket.on("new message", async (data) => {
         let conversation = await ConvoModel.findOne({
             $or: [
@@ -73,31 +85,61 @@ io.on("connection", async (socket) => {
 
         const saveMessage = await message.save();
 
-        const updatedConversation = await ConvoModel.updateOne ({
+        const updatedConversation = await ConvoModel.updateOne({
             _id: conversation._id
         }, {
             $push: { messages: saveMessage._id }
-        }, { new: true  
-        }) 
+        }, {
+            new: true
+        })
 
         const getConversation = await ConvoModel.findOne({
             $or: [
                 { sender: data.sender, receiver: data.receiver },
                 { sender: data.receiver, receiver: data.sender },
             ]
-        }).populate('messages').sort({updatedAt: -1});
+        }).populate('messages').sort({ updatedAt: -1 });
 
         io.to(data?.sender).emit('message', getConversation.messages);
         io.to(data.receiver).emit('message', getConversation.messages);
-    }); 
-
-
-    // Handle socket disconnection
-    socket.on("disconnect", () => {
-        onlineUser.delete(user?._id?.toString()); 
-        console.log("User disconnected:", socket.id);
     });
-});
 
-// Export the Express app and HTTP server
-module.exports = { app, server };
+    //sidebar
+    socket.on("sidebar", async (currrentUserId) => {
+        console.log('sidebar', currrentUserId)
+
+        const currrentUserConversation = await ConvoModel.find({
+            "$or": [
+                { sender: currrentUserId },
+                { receiver: currrentUserId }
+            ]
+        }).sort({ updatedAt: -1 }).populate('messages')
+        
+        // .populate('sender').populate('receiver');
+
+        const conversation = currrentUserConversation.map((conv)=>{
+            const countUnseenMsg = conv.messages.reduce((prev, curr) => prev + (curr.seen ? 0 : 1), 0);
+
+            return {
+                _id:conv?._id,
+                sender:conv?.sender,
+                receiver:conv?.receiver,
+                unseenMsg: countUnseenMsg,
+                lastMsg:conv?.messages[conv?.messages?.lenght - 1]
+            }
+        })
+
+        socket.emit('conversation',conversation)
+    })
+
+
+
+        // Handle socket disconnection
+        socket.on("disconnect", () => {
+            onlineUser.delete(user?._id);
+            console.log("User disconnected:", socket.id);
+        });
+    });
+
+    // Export the Express app and HTTP server
+    module.exports = { app, server };
