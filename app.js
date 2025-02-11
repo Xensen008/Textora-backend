63d19ed7a6c4cba674eb4482bb92119364373c0f
xensen008 <arnabjyotikakati008@gmail.com>
2025-02-11 12:50:42 +0530

    Update CORS configuration to support multiple origins and additional HTTP methods

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
